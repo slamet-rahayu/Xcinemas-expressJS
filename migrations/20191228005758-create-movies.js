b963e3ab3c0247e99ea69bb96ffee139bdd14344
slamet-rahayu <slamet_rahayu93@yahoo.com>
2020-01-13 00:12:48 +0700

    final commit

--- migrations/20191228005758-create-movies.js
+++ migrations/20191228005758-create-movies.js
@@ -8,9 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      theatre_id: {
-        type: Sequelize.INTEGER
-      },
       title: {
         type: Sequelize.STRING
       },
